6d5671da8b8b01d3bce0ef17ff51d4cd5af29015
Иванюк Константин <kon.ivanyuk@gmail.com>
2023-03-26 13:49:39 +0700

    + добавил модель

---
+++ Model/Note.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesListTask.Model
+{
+    public class Note : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _description;
+
+        public string Title
+        {
+            get 
+            { 
+                return _title; 
+            }
+
+            set 
+            {
+                _title = value; 
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; } 
+
+            set
+            {
+                _description= value; 
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
